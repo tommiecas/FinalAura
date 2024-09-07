5a23addcfc5136fea4a8c29831125f3499ed3229
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-09-07 16:19:26 -0700

    Elementalist Behavior Tree
    Signed-off-by: tommiecas <157588974+tommiecas@users.noreply.github.com>

--- Source/Aura/Public/Actors/AuraProjectile.h
+++ Source/Aura/Public/Actors/AuraProjectile.h
@@ -34,11 +34,12 @@ protected:
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USphereComponent> Sphere;
+	
 	bool bHit = false;
 private:
 
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
 
 	UPROPERTY(EditAnywhere)
 	TObjectPtr<UNiagaraSystem> ImpactEffect;
