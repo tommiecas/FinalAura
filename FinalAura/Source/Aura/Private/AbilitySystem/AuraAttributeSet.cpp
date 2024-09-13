8ddeab58967f65bd1730718bd88293ddb1f67c5a
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-09-13 13:33:11 -0700

    Sending XP Events
    Signed-off-by: tommiecas <157588974+tommiecas@users.noreply.github.com>

--- FinalAura/Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ FinalAura/Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -155,6 +155,8 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 				{
 					CombatInterface->Die();
 				}
+				SendXPEvent(Props);
+				
 			}
 			else
 			{
@@ -191,7 +193,23 @@ void UAuraAttributeSet::ShowFloatingText(const FEffectProperties& Props, const f
 		}
 		
 	}
-}	
+}
+
+void UAuraAttributeSet::SendXPEvent(const FEffectProperties& Props)
+{
+	if (ICombatInterface* CombatInterface = Cast<ICombatInterface>(Props.TargetCharacter))
+	{
+		const int32 TargetLevel = CombatInterface->GetPlayerLevel();
+		const ECharacterClass TargetClass = ICombatInterface::Execute_GetCharacterClass(Props.TargetCharacter);
+		const int32 XPReward = UAuraAbilitySystemLibrary::GetXPRewardForClassAndLevel(Props.TargetCharacter, TargetClass, TargetLevel);
+
+		const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
+		FGameplayEventData Payload;
+		Payload.EventTag = GameplayTags.Attributes_Meta_IncomingXP;
+		Payload.EventMagnitude = XPReward;
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(Props.SourceCharacter, GameplayTags.Attributes_Meta_IncomingXP, Payload);
+	}
+}
 
 /*
  ** Primary Attributes (Function Definitions)
